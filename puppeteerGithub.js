9578128d14efce99091d98008bb899e430178519
alxwndr <10734450+alxwndr@users.noreply.github.com>
2020-06-28 12:39:12 +0300

    created initialization script

---
+++ puppeteerGithub.js
@@ -0,0 +1,20 @@
+const github = require('./github');
+
+const login = 'email'; // email github
+const psw = 'pass'; // pass github
+const value = process.argv[2]; // Search value
+const nbUsers = process.argv[3]; // Nb user to scrap
+
+//$ node puppeteerGithub.js john 74
+
+(async () => {
+
+  await github.initialize();
+
+  await github.login(login, psw);
+
+  await github.search(value);
+
+  await github.getUsers(nbUsers);
+
+})()
